e4c07534e58d7597bc6f2ccacb191ae72224094c
prabhatmindfire16 <prabhat.jena@mindfiresolutions.com>
2020-09-25 20:02:47 +0530

    homepage component and router setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import HomePage from './homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
 
-import './App.css';
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <HomePage />
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/Menu-Item/menu-item.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, id, size, linkUrl, history, match }) => (
+  <div
+    id={id}
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className="content">
+      <div className="title">{title}</div>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default withRouter(MenuItem);
